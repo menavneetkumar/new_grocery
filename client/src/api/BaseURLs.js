0d75621955605688ef523964726d9d83e829513d
Navneet Kumar <menavneetkj@gmail.com>
2023-11-03 10:54:33 +0530

    product

--- client/src/api/BaseURLs.js
+++ client/src/api/BaseURLs.js
@@ -1,5 +1,5 @@
 export const PRODUCTS_BASEURL = 'https://rabbitmart-products.vercel.app/products'
 export const ORDERS_BASEURL = 'https://rabbitmart-orders.vercel.app/orders'
 export const SHIPPING_BASEURL = 'https://rabbitmart-shipping.vercel.app/shipping'
-export const PAYMENTS_BASEURL = 'https://new-grocery-payment.vercel.app/'
+export const PAYMENTS_BASEURL = 'https://new-grocery-payments.vercel.app/'
 export const USER_BASEURL = 'https://rabbitmart-users.vercel.app/me'
---
+++ product_file/data.jsx
@@ -0,0 +1,263 @@
+{
+    "total_pages": 18,
+
+        "products": [{
+            "_id": "62a5c098766f4ba646a28177",
+            "product_id": "6S8SWX",
+            "name": "Dairy Milk",
+            "price": 70,
+            "weight": 180,
+            "measurement": "gm",
+            "category": "Chocolate and Candy",
+            "image": "https://mir-s3-cdn-cf.behance.net/projects/404/bda152163495681.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.png",
+            "stock": 7,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a2818b",
+            "product_id": "PR5F1E",
+            "name": "Ferrero Rocher",
+            "price": 850,
+            "weight": 300,
+            "measurement": "gm",
+            "category": "Chocolate and Candy",
+            "image": "https://mir-s3-cdn-cf.behance.net/projects/404/281d59154172245.Y3JvcCwxNTIyLDExOTAsMCww.jpg",
+            "stock": 7, "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a28193",
+            "product_id": "QCJEX2",
+            "name": "Gems",
+            "price": 20,
+            "weight": 25,
+            "measurement": "gm",
+            "category": "Chocolate and Candy",
+            "image": "https://ranglamela.com/cdn/shop/files/Q10.jpg?v=1691980510",
+            "stock": 21,
+            "__v": 0
+        },
+
+
+        {
+            "_id": "62a5c098766f4ba646a281a9",
+            "product_id": "HKBREP",
+            "name": "Cadbury 5 Star",
+            "price": 85,
+            "weight": 100,
+            "measurement": "gm",
+            "category": "Chocolate and Candy",
+            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-8JA-SsW1-Dpytv2IxxdWH2c5fvc7NfRghQ&usqp=CAU",
+            "stock": 26,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281ba",
+            "product_id": "FCFVCW",
+            "name": "Sprite",
+            "price": 40,
+            "weight": 500,
+            "measurement": "ml",
+            "category": "Beverages",
+            "image": "https://planetroasters.com/wp-content/uploads/2017/04/sprite-canada.jpg",
+            "stock": 4,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281c0",
+            "product_id": "T3Z2D6",
+            "name": "Cocacola",
+            "price": 100,
+            "weight": 2, 
+            "measurement": "ltr",
+            "category": "Beverages",
+            "image": "https://cdn.cnn.com/cnnnext/dam/assets/220217102027-20220217-coke-new-flavor-super-tease.jpeg",
+            "stock": 38,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281c1",
+            "product_id": "W41E3S",
+            "name": "Maaza",
+            "price": 60,
+            "weight": 500,
+            "measurement": "ml",
+            "category": "Beverages",
+            "image": "https://5.imimg.com/data5/SELLER/Default/2023/3/KM/QU/XE/185952101/1-2-l-mazza-mango-drink.jpg",
+            "stock": 43,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281ca",
+            "product_id": "G8EQ79",
+            "name": "Pepsi",
+            "price": 40,
+            "weight": 250,
+            "measurement": "ml",
+            "category": "Beverages",
+            "image": "https://5.imimg.com/data5/XU/AE/HL/SELLER-82456434/pepsi-cold-drink.jpg",
+            "stock": 32,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281fe",
+            "product_id": "A70LXI",
+            "name": "Red Bull Energy Drink Sugar Free 250 Ml x",
+            "price": 90,
+            "weight": 250,
+            "measurement": "ml",
+            "category": "Beverages",
+            "image": "https://images.deliveryhero.io/image/product-information-management/61819ebc1b7474d5ab04231e.jpeg",
+            "stock": 46,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a28203",
+            "product_id": "QS1FKO",
+            "name": "Mr. Brown Natural Iced Coffee",
+            "price": 28.5,
+            "weight": 240,
+            "measurement": "ml",
+            "category": "Beverages",
+            "image": "https://images.deliveryhero.io/image/darkstores-QAtar/Products/QR_901923.jpeg",
+            "stock": 16,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a2820b",
+            "product_id": "RBWJV0",
+            "name": "Lays",
+            "price": 20,
+            "weight": 40,
+            "measurement": "gm",
+            "category": "Chips and Crackers",
+            "image": "https://www.straight.com/files/v3/styles/gs_standard/public/images/16/02/lineup-homecooked-d.png?itok=KQKLfFIG", 
+            "stock": 8,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a2824d",
+            "product_id": "RIFD4L",
+            "name": "Doritos",
+            "price": 60,
+            "weight": 550,
+            "measurement": "gm",
+            "category": "Chips and Crackers",
+            "image": "https://5.imimg.com/data5/SELLER/Default/2021/9/GL/FW/UD/113206078/doritos-tortilla-chips-bbq.jpg",
+            "stock": 42,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a2817e",
+            "product_id": "JUAG3R",
+            "name": "Chitale's Ice Cream",
+            "price": 200,
+            "weight": 150,
+            "measurement": "gm",
+            "category": "Ice cream",
+            "image": "https://chitales.in/img/home-background.png",
+            "stock": 28,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a28194",
+            "product_id": "D3FJE2",
+            "name": "Amul Ice Cream",
+            "price": 200,
+            "weight": 125,
+            "measurement": "gm",
+            "category": "Ice cream",
+            "image": "https://cdn.dotpe.in/longtail/store-items/5477836/oEetX2A8.jpeg",
+            "stock": 43,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281ae",
+            "product_id": "JBOTLO",
+            "name": "Katraj",
+            "price": 200,
+            "weight": 150,
+            "measurement": "gm",
+            "category": "Ice cream",
+            "image": "https://content3.jdmagicbox.com/comp/pune/x4/020pxx20.xx20.190514014502.z4x4/catalogue/katraj-dairy-milk-products-and-icecream-parlour-pune-milk-retailers-w2d4ko6iix.jpg",
+            "stock": 24,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281ce",
+            "product_id": "Y54AX4",
+            "name": "Mother Diary Icecream",
+            "price": 200,
+            "weight": 260,
+            "measurement": "gm",
+            "category": "Ice cream",
+            "image": "https://b.zmtcdn.com/data/pictures/chains/5/20553025/fddc678d48be65b7bc394d73a8d8b6fa.png?fit=around|960:500&crop=960:500;*,*",
+            "stock": 40,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281d1",
+            "product_id": "DRYC9Q",
+            "name": "Candy",
+            "price": 100,
+            "weight": 85,
+            "measurement": "gm",
+            "category": "Chocolate and Candy",
+            "image": "https://www.thecandybar.ca/cdn/shop/products/CandyBox-Sour_1024x1024.png?v=1676392588",
+            "stock": 25,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281e0",
+            "product_id": "S5RYFP",
+            "name": "Juhayna Pure Juice Pineapple",
+            "price": 23.45,
+            "weight": 1,
+            "measurement": "l",
+            "category": "Beverages",
+            "image": "https://images.deliveryhero.io/image/darkstores-eg/EGY_6222014335594.JPG",
+            "stock": 19,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281e1",
+            "product_id": "ADE4HX",
+            "name": "Juhayna Pure Juice Apple",
+            "price": 23.45,
+            "weight": 1, 
+            "measurement": "l",
+            "category": "Beverages",
+            "image": "https://images.deliveryhero.io/image/product-information-management/624975e43326f7f331b40023.jpg",
+            "stock": 21,
+            "__v": 0
+        },
+
+        {
+            "_id": "62a5c098766f4ba646a281ec",
+            "product_id": "WMPW6Z",
+            "name": "Lamar Pomegranate Juice 100%",
+            "price": 23.95,
+            "weight": 1,
+            "measurement": "l",
+            "category": "Beverages",
+            "image": "https://images.deliveryhero.io/image/darkstores-eg/EGY_6224001105517.JPG",
+            "stock": 22,
+            "__v": 0
+        }]
+}
